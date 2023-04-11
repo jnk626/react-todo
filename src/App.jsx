12f4d822a82ccd7336b7691deef72795af439bad
jnk626 <jnk626@pm.me>
2023-04-11 09:00:52 +0200

    deletes unnecessary svgs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [newItem, setNewItem] = useState("");
@@ -8,77 +8,83 @@ function App() {
   function handlerSubmit(e) {
     e.preventDefault();
 
-    setTodos(currentTodos => {
+    setTodos((currentTodos) => {
       return [
         ...currentTodos,
         {
           id: crypto.randomUUID(),
           title: newItem,
-          completed: false
-        }
-      ]
-    })
+          completed: false,
+        },
+      ];
+    });
 
     setNewItem("");
   }
 
   function toggleTodo(id, completed) {
-    setTodos(currentTodos => {
-      return currentTodos.map(todo => {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
         if (todo.id === id) {
-          return {...todo, completed}
+          return { ...todo, completed };
         }
 
         return todo;
-      })
-    }
-
-    )
+      });
+    });
   }
 
   function deleteTodo(id) {
-    setTodos(currentTodos => {
-      return currentTodos.filter(todo => todo.id !== id);
-    })
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
   }
 
   console.log(todos);
   // fragment <> </>
-  return <> 
-  <form onSubmit={handlerSubmit}>
-    <label className="labels" htmlFor="search">New Item</label>
-    <input 
-    value={newItem}
-    onChange={e => setNewItem(e.target.value)}
-    name="search" 
-    id="search"  />
-    <button className='btn'>Aggiungi</button>
-  </form>
-  <h1 className='title'>Todo List</h1>
-  <ul>
-    {todos.length == 0 && "No todos yet"}
-    {todos.map(todo => {
-      return (
-      <li key={todo.id}>
-        <label key={todo.id}>
-          <input 
-          type="checkbox" 
-          checked={todo.checked}
-          onChange={e => toggleTodo(todo.id, e.target.checked)} />
-          {todo.title}
+  return (
+    <>
+      <form onSubmit={handlerSubmit}>
+        <label className="labels" htmlFor="search">
+          New Item
         </label>
-        
-        <button 
-        className='btn delete-btn' 
-        onClick={() => deleteTodo(todo.id)}>
-          Delete
-        </button> 
-      </li>)
-    }) }
-  </ul>
-  </>
+        <input
+          value={newItem}
+          onChange={(e) => setNewItem(e.target.value)}
+          name="search"
+          id="search"
+        />
+        <button className="btn">Aggiungi</button>
+      </form>
+      <h1 className="title">Todo List</h1>
+      <ul>
+        {todos.length == 0 && "No todos yet"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label key={todo.id}>
+                <input
+                  type="checkbox"
+                  checked={todo.checked}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+
+              <button
+                className="btn delete-btn"
+                onClick={() => deleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
   //line 61: for React, eache element must have a unique, non-progressive id
   //line 72: lambda because I want it to execute, not to have the result
-  }
+}
 
-export default App
+export default App;
